dffde8dcc1336e2ddc727561d715cdd79d1fd582
JunHeeCh <apdlsid1@naver.com>
2026-08-10 15:02:24 +0900

    Initial Commit

---
+++ CountdownCat/CountdownCatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownCatApp.swift
+//  CountdownCat
+//
+//  Created by cho on 8/10/26.
+//
+
+import SwiftUI
+
+@main
+struct CountdownCatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
